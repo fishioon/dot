30b0a10dcf1138c27a99e2bb86d52fc807292023
ifish <fishioon@live.com>
2025-06-16 14:41:13 +0800

    update

--- fish/env.fish
+++ fish/env.fish
@@ -33,3 +33,8 @@ if test -e "$XDG_CONFIG_HOME/work/work.env"
 end
 
 export KUBECONFIG=$HOME/.kube/config:$HOME/.kube/sealos.yaml
+
+export HOMEBREW_API_DOMAIN="https://mirrors.aliyun.com/homebrew-bottles/api"
+export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.aliyun.com/homebrew/brew.git"
+export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.aliyun.com/homebrew/homebrew-core.git"
+export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.aliyun.com/homebrew/homebrew-bottles"
