7e048eef59f4401cd0a45ea4e57921eebc1575fa
ifish <fishioon@live.com>
2025-06-19 14:45:10 +0800

    update

--- fish/env.fish
+++ fish/env.fish
@@ -35,6 +35,6 @@ end
 export KUBECONFIG=$HOME/.kube/config:$HOME/.kube/sealos.yaml
 
 export HOMEBREW_API_DOMAIN="https://mirrors.aliyun.com/homebrew-bottles/api"
-export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.aliyun.com/homebrew/brew.git"
+# export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.aliyun.com/homebrew/brew.git"
 export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.aliyun.com/homebrew/homebrew-core.git"
 export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.aliyun.com/homebrew/homebrew-bottles"
